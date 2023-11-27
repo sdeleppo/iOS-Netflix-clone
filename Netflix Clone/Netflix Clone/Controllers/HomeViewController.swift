d0b7f6ea855c2f495f2ce665b28dd50612a4e793
sdeleppo <sdeleppo@yahoo.com>
2023-11-27 14:22:48 -0500

    Setting up TableView and home tab structure

--- Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
@@ -3,12 +3,14 @@
 //  Netflix Clone
 //
 //  Created by Sarah Deleppo on 12/27/22.
-//
 
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    let sectionTitles: [String] = ["Trending Movies", "Popular", "Trending TV", "Top Rated", "Upcoming Movies"]
+    
+    var lastOffset:CGFloat = 0
+    var offset:CGFloat = 0
     private let homeFeedTable: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped);
         table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
@@ -24,7 +26,25 @@ class HomeViewController: UIViewController {
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
         
-        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 400))
+        configureNavbar()
+        
+        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 530))
+        homeFeedTable.tableHeaderView = headerView
+    }
+    
+    private func configureNavbar() {
+        var image = UIImage(named: "netflixLogo")
+        
+        //UIImage(systemName: "person")
+        //UIImage(named: "netflixLogo")
+        
+        image = image?.withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        navigationController?.navigationBar.tintColor = .white
     }
     
     override func viewDidLayoutSubviews() {
@@ -36,9 +56,8 @@ class HomeViewController: UIViewController {
     
 }
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
-    
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
+        return sectionTitles.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -58,5 +77,33 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else {return}
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .white
+        header.textLabel?.text = header.textLabel?.text?.lowercased()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+
+        if ((self.lastOffset < scrollView.contentOffset.y) && scrollView.contentOffset.y >= 5) {
+            if (self.offset < 100 && self.offset >= 0) {
+                self.offset += 10
+            }
+            navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -self.offset))
+        } else if ((self.lastOffset > scrollView.contentOffset.y) && scrollView.contentOffset.y != 0) {
+            if (self.offset <= 100 && self.offset > 0) {
+                self.offset -= 10
+            }
+            navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -self.offset))
+        }
+        self.lastOffset = scrollView.contentOffset.y
     }
 }
